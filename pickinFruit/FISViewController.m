60907707c70dfcabbf60548fa410191ea3916c12
zdrossman <zachary.drossman@gmail.com>
2014-09-17 16:29:10 -0400

    Added solution.

--- pickinFruit/FISViewController.m
+++ pickinFruit/FISViewController.m
@@ -10,29 +10,84 @@
 
 @interface FISViewController ()
 
+@property (strong, nonatomic) UIAlertView *spinnerAlertView;
+
 - (IBAction)spin:(id)sender;
 
 @end
 
 @implementation FISViewController
 
+static NSInteger const numberOfComponents = 3;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 
+    self.fruitPicker.delegate = self;
+    self.fruitPicker.dataSource = self;
+    
     self.fruitsArray = @[@"Apple",@"Orange",@"Banana",@"Pear",@"Grape", @"Kiwi", @"Mango", @"Blueberry", @"Raspberry"];
+    
+    self.fruitPicker.accessibilityLabel = @"FruitSpinner";
+    self.fruitPicker.accessibilityIdentifier = @"FruitSpinner";
+    
+    self.spinButton.accessibilityLabel = @"SpinButton";
+    self.spinButton.accessibilityIdentifier = @"SpinButton";
+    
+
 }
 
-- (void)didReceiveMemoryWarning
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    return numberOfComponents;
 }
 
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return [self.fruitsArray count];
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return self.fruitsArray[row];
+}
+
+- (NSInteger)getRandomFruit {
+    return arc4random_uniform([self.fruitsArray count]);
+}
 
 - (IBAction)spin:(id)sender {
-
-#warning Implement this method!
+    
+    [self.fruitPicker selectRow:[self getRandomFruit] inComponent:0 animated:YES];
+    [self.fruitPicker selectRow:[self getRandomFruit] inComponent:1 animated:YES];
+    [self.fruitPicker selectRow:[self getRandomFruit] inComponent:2 animated:YES];
+    
+    NSString *spinResultMessage = @"You lost!";
+    
+    NSString *fruitOne = self.fruitsArray[[self.fruitPicker selectedRowInComponent:0]];
+    NSString *fruitTwo = self.fruitsArray[[self.fruitPicker selectedRowInComponent:1]];
+    NSString *fruitThree = self.fruitsArray[[self.fruitPicker selectedRowInComponent:2]];
+    
+    if ( [fruitOne isEqualToString:fruitTwo] && [fruitTwo isEqualToString:fruitThree])
+    {
+        spinResultMessage = @"You Won!";
+    }
+    
+    self.spinnerAlertView = [[UIAlertView alloc] initWithTitle:@"Drumroll please..." message:spinResultMessage delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Re-spin", nil];
+    
+    self.spinnerAlertView.delegate = self;
+    
+    [self.spinnerAlertView show];
+    
 }
+
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == 1) {
+        [self spin:nil];
+    }
+}
+
 @end
